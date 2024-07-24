7e0b5855580c831de8ee735831868af1f8f0fa2f
aurele <aurelien.plazzotta@tutanota.com>
2024-07-24 15:03:33 +0200

    Added logout feature

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -22,5 +23,13 @@ class LoginController extends AbstractController
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logOut(Security $security): Response
+    {
+        // Log out the user on the current firewall.
+        $response = $security->logout();
+        return $this->redirectToRoute('app_homepage', [], Response::HTTP_SEE_OTHER);
     }
 }
